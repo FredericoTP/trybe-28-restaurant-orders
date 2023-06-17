27901e0ee0003344bed1edba788d8e25a3085459
Frederico Tavares <fredericotp@hotmail.com>
2023-06-17 10:14:47 -0300

    Feat: Requisito 1 - Testes classe Ingredient

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,22 @@
 from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+import pytest
 
 
 # Req 1
 def test_ingredient():
-    pass
+    new_salmao = Ingredient("salmão")
+    new_salmao2 = Ingredient("salmão")
+    new_farinha = Ingredient("farinha")
+
+    assert new_salmao.name == "salmão"
+    assert new_salmao.__repr__() == "Ingredient('salmão')"
+    assert new_salmao.__hash__() == new_salmao2.__hash__()
+    assert new_salmao.__hash__() != new_farinha.__hash__()
+    assert new_salmao.__eq__(new_salmao2) is True
+    assert new_salmao.__eq__(new_farinha) is False
+    assert new_salmao.restrictions
+    assert isinstance(new_farinha.restrictions, set)
+
+    with pytest.raises(TypeError):
+        new_ingredient = Ingredient()
+        new_ingredient.name
